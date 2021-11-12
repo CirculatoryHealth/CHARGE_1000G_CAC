a5f9b64551b24f534bd8dfa55f9ab1222c1bcdb6
swvanderlaan <s.w.vanderlaan@gmail.com>
2021-11-12 10:34:56 +0100

    updated analyses for 853 SNPs

---
+++ SNP/CAC_overallplaque/snptest_results/Collagen/cleaner.AEGS.VARIANT.Collagen.EXCL_DEFAULT.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#
+/hpc/local/CentOS7/dhl_ec/software/GWASToolKit/gwastoolkit.cleaner.sh /hpc/dhl_ec/svanderlaan/projects/lookups/AE_20200512_COL_MKAVOUSI_MBOS_CHARGE_1000G_CAC/SNP/gwastoolkit.conf Collagen 
